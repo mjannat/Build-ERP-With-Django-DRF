07f98fc4c907138cb5e9fe4ac5a95ce9e42178ac
Mim Jannat <mjannat435@gmail.com>
2023-09-06 12:27:22 +0600

    Rest Framework Initialize

--- src/core/views.py
+++ src/core/views.py
@@ -1,11 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 
-def test_view(request):
-    data = {
-        'name': 'Mim Jannat',
-        'age' : 20
-    }
-    return JsonResponse(data)
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+        
+            'name' : 'Mim Jannat',
+            'age' : 25
+        }
+        return Response()
+# def test_view(request):
+#     data = {
+#         'name': 'Mim Jannat',
+#         'age' : 20
+#     }
+#     return JsonResponse(data)
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import test_view
+from django.urls import include, path
+
+from core.views import TestView
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('a/', test_view),
+    path('', TestView.as_view()),
 ]
